8ee03038c0b878156b540946f42fec64e367acbf
Godswill Uduozor <uduozor962@gmail.com>
2026-06-01 21:22:04 +0100

    App.css, Hero and VideoSection by Mrwills

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-import image from "../assets/planet_img.png";
+import img from "../assets/planet_img.png";
 function Hero() {
   const scrollToPlanets = () => {
     const section = document.getElementById("planets");
@@ -51,7 +51,7 @@ function Hero() {
 
         <div className="hero-image">
           <img
-            src={image}
+            src={img}
             alt="Planet Earth"
           />
         </div>
